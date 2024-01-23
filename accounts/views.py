474f0163c6c5e436756351841cf23c449c22920a
unknown <kibum0209@naver.com>
2024-01-23 19:53:28 +0900

    3주차 오류 수정

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,3 @@
-from django.shortcuts import render
-
-# Create your views here.
 from django.contrib.auth import login
 from django.shortcuts import render, redirect
 from accounts.forms import SignupForm
@@ -12,6 +9,6 @@ def signup(request):
               user = form.save()
               login(request, user)
               return redirect('/')
-         else:
-              form = SignupForm()
-         return render(request, 'accounts/signup.html', {'form': form})
+    else:
+        form = SignupForm()
+    return render(request, 'accounts/signup.html', {'form': form})
